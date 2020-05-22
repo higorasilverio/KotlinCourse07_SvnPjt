931d9e3465f614dda976af72a99cd1385fe2834c
higorasilverio <higorasilverio@outlook.com>
2020-05-22 17:05:12 -0300

    Layout and navigation between pages - final

---
+++ app/src/main/java/br/com/silverio/kotlincourse07_svnpjt/DetalheContatoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.silverio.kotlincourse07_svnpjt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheContatoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_contato)
+    }
+}
--- app/src/main/java/br/com/silverio/kotlincourse07_svnpjt/MainActivity.kt
+++ app/src/main/java/br/com/silverio/kotlincourse07_svnpjt/MainActivity.kt
@@ -1,12 +1,36 @@
 package br.com.silverio.kotlincourse07_svnpjt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_contato.setOnClickListener {
+            val intent = Intent(this, DetalheContatoActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_empresa.setOnClickListener {
+            val intent = Intent(this, DetalheEmpresaActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_clientes.setOnClickListener {
+            val intent = Intent(this, DetalheClienteActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_servicos.setOnClickListener {
+            val intent = Intent(this, DetalheServicoActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
